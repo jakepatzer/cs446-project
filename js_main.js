39d568b6a9f720489a2e80b01400ce803fc289db
nilsenjon <57730020+nilsenjon@users.noreply.github.com>
2019-12-05 22:14:36 -0800

    Add files via upload
    very basic start

---
+++ js_main.js
@@ -0,0 +1,43 @@
+let weekDays = [
+  "Sunday", "Monday", "Tuesday", "Wednesday",
+  "Thursday", "Friday", "Saturday"
+];
+
+let date = 1;
+
+function generateButtons(data, date) {
+  let date_temp = date;
+  var more=document.getElementById("week_list");
+  for (let i = 0; i < data.length-1; i++) {
+    var btn=document.createElement("button");
+    btn.innerHTML=data[i] + " Dec " +date_temp;
+    date_temp += 1;
+    more.appendChild(btn);
+  }
+}
+
+function generateTopButtons(){
+  var location = document.getElementById("top_buttons");
+  var lbtn=document.createElement("button");
+  lbtn.id = "bL";
+  var rbtn=document.createElement("button");
+  rbtn.id = "bR";
+  location.appendChild(lbtn);
+  location.appendChild(rbtn);
+  var buttonL = document.getElementById("bL");
+  buttonL.innerHTML = '<img id="img" src="arrowL.jpg" />';
+  var buttonR = document.getElementById("bR");
+  buttonR.innerHTML = '<img id="img" src="arrowR.jpg" />';
+}
+
+function leftArrowClicked(){
+  
+}
+
+
+
+
+
+
+generateTopButtons();
+generateButtons(weekDays, date);
